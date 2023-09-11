a6973ac1c08bf699525246065b9319df33ba6db2
Aadityakiran S <aadityakirans@gmail.com>
2023-09-11 11:17:14 +0530

    Installed and tested out sequalize ORM

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/router.js');
-const { urlencoded } = require('express');
 require('dotenv').config();
 
 //using the json parser
